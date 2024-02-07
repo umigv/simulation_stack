70c6201585bc92d4e781b7b6987ff5c8ed091ccf
Ryan <ryan.liao0305@gmail.com>
2024-02-07 16:22:06 -0500

    Remove nav2 from installation script

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -3,7 +3,7 @@ set -e
 
 # Dependencies
 sudo apt update
-sudo apt-get install ros-humble-rttest ros-humble-rclcpp-action ros-humble-gazebo-dev ros-humble-gazebo-msgs ros-humble-gazebo-plugins ros-humble-gazebo-ros ros-humble-gazebo-ros-pkgs ros-humble-joint-state-publisher-gui ros-humble-xacro ros-humble-nav2-core ros-humble-nav2-bringup
+sudo apt-get install ros-humble-rttest ros-humble-rclcpp-action ros-humble-gazebo-dev ros-humble-gazebo-msgs ros-humble-gazebo-plugins ros-humble-gazebo-ros ros-humble-gazebo-ros-pkgs ros-humble-joint-state-publisher-gui ros-humble-xacro
 
 # Submodules
 git submodule update --init --recursive 
